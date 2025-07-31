6a589fdc2856f3e9468d4c87006eaa2bfedd0145
Chris Pagolu <chrispagolu@gmail.com>
2025-08-01 00:24:30 +1000

    Initial Commit

---
+++ AirLinkTests/AirLinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirLinkTests.swift
+//  AirLinkTests
+//
+//  Created by Chris on 8/1/25.
+//
+
+import Testing
+@testable import AirLink
+
+struct AirLinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
